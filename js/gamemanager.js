f1cd4c69f006644e1a163ee1408fa17e36772b20
Raul Rodriguez <raul.i.rodriguez@gmail.com>
2021-01-09 02:05:19 -0600

    documentation

--- js/gamemanager.js
+++ js/gamemanager.js
@@ -43,9 +43,9 @@ let GameManager = {
 
         //declare the enemy types 
         let enemy00 = new Enemy("Goblin", 100,0,50,100,100);
-        let enemy01 = new Enemy("Troll", 150,0,75,75,75);
-        let enemy02 = new Enemy("Orc",200,0,200,50,50);
-        let enemy03 = new Enemy("Valkyrie",125,25,100,25,25);
+        let enemy01 = new Enemy("Fallen", 150,0,75,75,75);
+        let enemy02 = new Enemy("Boggit",200,0,200,50,50);
+        let enemy03 = new Enemy("Wraith",125,25,100,25,25);
 
         //Logic to randomly select an enemy to fight in this dungeon battle
         let randomEnemySelector = Math.floor(Math.random() * Math.floor(4));
@@ -61,10 +61,10 @@ let GameManager = {
             case "3": enemy = enemy03;
             break;
         }
-        //assign html statements for Enemy to the DOM
+        //assign html statements for different attacks to the DOM
         getHeader.innerHTML  = '<p>Task: Choose Your Attack</p>';
         getAttacks.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()">Physical Attack!</a>';
         //build the enemy in the DOM starting with the enemy's image, the name of the enemy in a header, and then a status box of the enemy's vitals
-        getEnemy.innerHTML   = '<img src="img/enemy-avatars/' + enemy.enemyType.toLowerCase() + '.jpeg" alt="' + enemy.enemyType + '"class="img-avatar"><div><h3>' + enemy.enemyType + '</h3><p class="health-enemy">Health: ' + enemy.health + '</p><p>Mana: ' + enemy.mana + '</p><p>Strength: ' + enemy.strength + '</p><p>Agility: ' + enemy.agility + '</p><p>Speed: ' + enemy.speed + '</p></div>';
+        getEnemy.innerHTML = '<img src=".img/enemy-avatars/' + enemy.enemyType.toLowerCase() + '.jpeg " alt="' + enemy.enemyType + ' "class="img-avatar"><div><h3>' + enemy.enemyType + '</h3><p class="enemy-health">Health: ' + enemy.health + '</p><p>Mana: ' + enemy.mana + '</p><p>Strength: ' + enemy.strength + '</p><p>Agility: ' + enemy.agility + '</p><p>Speed: ' + enemy.speed + '</p></div>';
     }
     } 
